f16838aa291078553282a254a780ddd3de0df867
Tobias <t.forner@tum.de>
2023-12-07 16:00:04 +0100

    add day 3

---
+++ src/day03.rs
@@ -0,0 +1,104 @@
+use logos::Logos;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos(skip r"[.]")]
+enum Token {
+    // Or regular expressions.
+    #[regex("[0-9]+", |lex|lex.slice().parse().ok())]
+    Number(u32),
+    #[regex(r"[*+/&=\-@#%$]", |lex|lex.slice().chars().next().unwrap())]
+    Special(char),
+}
+
+fn surrounding_positions(line: i64, start: i64, end: i64) -> impl Iterator<Item = (i64, i64)> {
+    let around = start - 1..=end;
+    let left_right = [(line, start - 1), (line, end)];
+    return around
+        .clone()
+        .map(move |x| (line - 1, x))
+        .chain(left_right.into_iter())
+        .chain(around.map(move |x| (line + 1, x)));
+}
+
+fn part1(text: &str) {
+    let lines: Vec<&str> = text.lines().collect();
+
+    let mut numbers: Vec<(i64, i64, i64, u32)> = vec![];
+    let mut specials = HashSet::new();
+
+    lines.iter().enumerate().for_each(|(i, l)| {
+        let lex = Token::lexer(l);
+        lex.spanned().for_each(|(t, s)| {
+            match t {
+                Ok(Token::Number(n)) => {
+                    numbers.push((i as i64, s.start as i64, s.end as i64, n));
+                }
+                Ok(Token::Special(_)) => {
+                    specials.insert((i as i64, s.start as i64));
+                }
+                Err(()) => panic!("f"),
+            };
+        })
+    });
+
+    let res: u32 = numbers
+        .iter()
+        .map(|(line, start, end, num)| {
+            if surrounding_positions(*line, *start, *end).any(|p| specials.contains(&p)) {
+                *num
+            } else {
+                0
+            }
+        })
+        .sum();
+    println!("{res}");
+}
+
+fn part2(text: &str) {
+    let lines: Vec<&str> = text.lines().collect();
+
+    let mut numbers: Vec<(i64, i64, i64, u32)> = vec![];
+    let mut specials: HashMap<(i64, i64), Vec<u32>> = HashMap::new();
+
+    lines.iter().enumerate().for_each(|(i, l)| {
+        let lex = Token::lexer(l);
+        lex.spanned().for_each(|(t, s)| {
+            match t {
+                Ok(Token::Number(n)) => {
+                    numbers.push((i as i64, s.start as i64, s.end as i64, n));
+                }
+                Ok(Token::Special(sp)) => {
+                    if sp == '*' {
+                        specials.insert((i as i64, s.start as i64), vec![]);
+                    }
+                }
+                Err(()) => panic!("f"),
+            };
+        })
+    });
+
+    numbers.iter().for_each(|(l, s, e, n)| {
+        surrounding_positions(*l, *s, *e).for_each(|(s, e)| {
+            if let Some(v) = specials.get_mut(&(s, e)) {
+                v.push(*n);
+            }
+        })
+    });
+
+    let res: u32 = specials
+        .iter()
+        .filter(|(_, v)| v.len() == 2)
+        .map(|(_, v)| v[0] * v[1])
+        .sum();
+    println!("{res}");
+}
+
+pub fn compute() {
+    let text = fs::read_to_string("inputs/day03.txt").expect("expected readable file");
+    part1(&text);
+    part2(&text);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 mod day01;
 mod day02;
 mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
 
 use clap::Parser;
 
-///rtnertn
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -19,19 +14,8 @@ fn main() {
     let args = Args::parse();
     match args.day {
         1 => day01::compute(),
-        2 => day02::compute(),
+        2 => day02::compute().expect("should not fail!"),
         3 => day03::compute(),
-        4 => day04::compute(),
-        5 => day05::compute(),
-        6 => day06::compute(),
-        7 => day07::compute(),
         _ => panic!("Invalid Day!"),
     }
-    //day01::compute();
-    //day02::compute();
-    //day03::compute();
-    //day04::compute();
-    //day05::compute();
-    //day06::compute();
-    day07::compute();
 }
