39327ade39a7b3d192f8aca3b785f06c0a565b20
Tobias <t.forner@tum.de>
2023-12-08 16:47:20 +0100

    add day 6

---
+++ src/day06.rs
@@ -0,0 +1,106 @@
+use anyhow::Result;
+use std::fs;
+
+fn parse_input(text: &str) -> Result<Vec<(u64, u64)>> {
+    let vecs = text
+        .lines()
+        .map(|l| {
+            let mut nums = l.split_ascii_whitespace();
+            nums.next();
+            nums.map(|n| n.parse().unwrap()).collect::<Vec<u64>>()
+        })
+        .collect::<Vec<Vec<u64>>>();
+    let res = vecs[0]
+        .clone()
+        .into_iter()
+        .zip(vecs[1].clone().into_iter())
+        .collect();
+    Ok(res)
+}
+
+fn part1(text: &str) {
+    let input = parse_input(text).unwrap();
+    let res = input
+        .iter()
+        .map(|(t, d)| (1..=t - 1).filter(|p| is_winning(*p, *t, *d)).count())
+        .reduce(|acc, c| acc * c)
+        .unwrap();
+    println!("part 1: {res}");
+}
+
+fn lowest_winning(start: u64, end: u64, time: u64, dist: u64) -> Option<u64> {
+    let mid = (start + end).div_ceil(2);
+    //println!("{start}; {end}; {mid}");
+    if is_winning(mid, time, dist) {
+        if start >= end {
+            return Some(mid);
+        }
+        if start == end - 1 {
+            if is_winning(start, time, dist) {
+                return Some(start);
+            } else {
+                return Some(mid);
+            }
+        }
+        return lowest_winning(start, mid, time, dist);
+    } else {
+        if start >= end {
+            return None;
+        }
+        let lower = lowest_winning(start, mid.saturating_sub(1), time, dist);
+        if lower.is_some() {
+            return lower;
+        }
+        lowest_winning(mid + 1, end, time, dist)
+    }
+}
+
+fn is_winning(press_time: u64, time: u64, dist: u64) -> bool {
+    (time - press_time) * press_time > dist
+}
+fn largest_winning(start: u64, end: u64, time: u64, dist: u64) -> Option<u64> {
+    let mid = (start + end).div_ceil(2);
+    //println!("{start}; {end}; {mid}");
+    if is_winning(mid, time, dist) {
+        if start >= end {
+            return Some(mid);
+        }
+        return largest_winning(mid, end, time, dist);
+    } else {
+        if start >= end {
+            return None;
+        }
+        let upper = largest_winning(mid + 1, end, time, dist);
+        if upper.is_some() {
+            return upper;
+        }
+        largest_winning(start, mid.saturating_sub(1), time, dist)
+    }
+}
+
+fn part2(text: &str) {
+    let numbers: Vec<u64> = text
+        .lines()
+        .map(|l| {
+            l.chars()
+                .filter(|c| c.is_ascii_digit())
+                .collect::<String>()
+                .parse::<u64>()
+                .unwrap()
+        })
+        .collect();
+    println!("{numbers:?}");
+    let lowest = lowest_winning(1, numbers[0] - 1, numbers[0], numbers[1]);
+    let largest = largest_winning(1, numbers[0] - 1, numbers[0], numbers[1]);
+    if let (Some(n), Some(m)) = (lowest, largest) {
+        let res = m - n + 1;
+        println!("part 2: {res}");
+    }
+    println!("{lowest:?}; {largest:?}");
+}
+
+pub fn compute() {
+    let text = fs::read_to_string("inputs/day06.txt").expect("expected readable file");
+    part1(&text);
+    part2(&text);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 use clap::Parser;
 
@@ -20,6 +21,7 @@ fn main() {
         3 => day03::compute(),
         4 => day04::compute(),
         5 => day05::compute(),
+        6 => day06::compute(),
         _ => panic!("Invalid Day!"),
     }
 }
